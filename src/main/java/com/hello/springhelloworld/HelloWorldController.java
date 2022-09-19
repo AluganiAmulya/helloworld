ac024e24350d11a0bf4f74497a7b98357075d168
unknown <mindc1july15@iiht.tech>
2022-09-19 09:39:11 +0530

    First commit

---
+++ src/main/java/com/hello/springhelloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.hello.springhelloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+		@RequestMapping("/")
+		public String hello()
+		{
+			return "Hello World";
+		}
+	}
---
+++ src/test/java/com/hello/springhelloworld/SpringhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.springhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringhelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
